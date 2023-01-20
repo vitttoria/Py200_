9843d8c5cefb196ac6df64e435cd511e74d20811
vitttoria <115340422+vitttoria@users.noreply.github.com>
2023-01-20 18:18:08 +0300

    Update task1.py

--- Lab_1/task1.py
+++ Lab_1/task1.py
@@ -3,11 +3,26 @@ import doctest
 
 class Book:
     def __init__(self, pages: int, author: str):
+        """
+             Создание и подготовка к работе объекта "Книга"
+             :pages capacity_volume: Объем стакана
+             :author occupied_volume: Объем занимаемой жидкости
+             Примеры:
+             >>> book2 = Book(500, 'John')  # инициализация экземпляра класса
+             """
         self.pages = pages
         self.author = author
         self.get_pages(pages)
 
     def get_pages(self, pages: int):
+        """
+                Функция которая проверяет являются ли страницы числом
+                :raise TypeError: Если значение не является числом, то вызывается ошибка
+                :raise ValueError: Если значение отрицательное, то вызывается ошибка
+                Примеры:
+                >>> book1 = Book(500, 'Mary')
+                >>> book.get_pages(-300)
+                """
         if not isinstance(pages, int):
             raise TypeError("Значение не является численным")
         if pages < 0:
@@ -19,13 +34,15 @@ class Book:
 
 class Car:
     def __init__(self, color: str, size: int):
+        """
+             Создание и подготовка к работе объекта "Машина"
+             :pages color: Цвет
+             :author size: Размер
+             Примеры:
+             >>> car1 = Car('Yellow', 5)  # инициализация экземпляра класса
+             """
         self.color = color
         self.size = size
-        self.get_color(color)
-
-    def get_color(self, color: str):
-        # color = input("Введите цвет\n")
-        return color
 
     def __str__(self):
         return f'Цвет машины: {self.color}, Размер: {self.size}'
@@ -33,11 +50,26 @@ class Car:
 
 class Table:
     def __init__(self, color: str, hight: int):
+        """
+             Создание и подготовка к работе объекта "Стол"
+             :pages color: Цвет
+             :author hight: Высота
+             Примеры:
+             >>> car1 = Car('Yellow', 5)  # инициализация экземпляра класса
+             """
         self.hight = hight
         self.color = color
         self.get_hight(hight)
 
     def get_hight(self, hight: int):
+        """
+                Функция которая проверяет является ли высота числом
+                :raise TypeError: Если значение не является числом, то вызывается ошибка
+                :raise ValueError: Если значение отрицательное, то вызывается ошибка
+                Примеры:
+                >>> table1 = Table('Yellow', 6)
+                >>> table1.get_hight(-4)
+                """
         if not isinstance(hight, int):
             raise TypeError("Значение не является численным")
         if hight < 0:
